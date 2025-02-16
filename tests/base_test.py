c69e936f5a133b83d772fae30d267f0376183554
acecka <agn.cecka@gmail.com>
2025-02-16 09:33:01 +0100

    Initial repo

---
+++ tests/base_test.py
@@ -0,0 +1,16 @@
+import unittest
+from selenium import webdriver
+from pages.home_page import HomePage
+
+class BaseTest(unittest.TestCase):
+    """
+    Base class for each test
+    """
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get("https://demoblaze.com/")
+        self.home_page = HomePage(self.driver)
+
+    def tearDown(self):
+        self.driver.quit()
